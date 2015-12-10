13e82d9986ce5111cfb0d6dc46b8bdd77624b59f
Toalei.Zhou <568797992@qq.com>
2015-12-10 18:30:21 +0800

    注册广播

--- src/com/coolweathers/app/activity/WeatherActivity.java
+++ src/com/coolweathers/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.coolweathers.app.activity;
 
 import com.coolweathers.app.R;
+import com.coolweathers.app.service.AutoUpdateService;
 import com.coolweathers.app.util.HttpCallbackListener;
 import com.coolweathers.app.util.HttpUtil;
 import com.coolweathers.app.util.Utility;
@@ -153,6 +154,8 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this,AutoUpdateService.class);
+		startService(intent);
 	}
 	@Override
 	public void onClick(View v) {
---
+++ src/com/coolweathers/app/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,17 @@
+package com.coolweathers.app.receiver;
+
+import com.coolweathers.app.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context,AutoUpdateService.class);
+		context.startService(i);
+	}
+
+}
